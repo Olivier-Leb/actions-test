cfdab974d8040127cae7e5d449ee3c95a6807a81
O <>
2024-03-04 11:36:16 -0500

    New test

--- src/test/java/example/github/actions/ActionTest.java
+++ src/test/java/example/github/actions/ActionTest.java
@@ -2,10 +2,17 @@ package example.github.actions;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ActionTest {
 
     @Test
     public void testSimple() {
         System.out.println("Hello world!");
+    }
+
+    @Test
+    public void testAssert() {
+        assertTrue(true, "Junit KO");
     }
 }
