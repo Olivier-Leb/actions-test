f726eb20ac42eb28cb5d2e149d6aa35bcda79514
Olivier-Leb <>
2024-03-05 15:14:17 -0500

    Cleanup tests

--- src/test/java/example/github/actions/ActionTest.java
+++ src/test/java/example/github/actions/ActionTest.java
@@ -11,16 +11,6 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ActionTest {
 
     @Test
-    public void testKO() {
-        assertTrue(false, "test KO");
-    }
-
-    @Test
-    public void testSimple() {
-        System.out.println("Hello world!");
-    }
-
-    @Test
     public void testAssert() {
         assertTrue(true, "Junit KO");
     }
